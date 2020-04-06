e0de230a8a56e67f02ab2ddaa873b93e236f8686
Gökhan Karaçavuş <gokhankaracavus@hotmail.com>
2020-04-07 02:48:20 +0300

    Toplama tamamlandı

---
+++ HesaplamaApp/src/main/java/tr/edu/medipol/yazgel/Hesap.java
@@ -0,0 +1,8 @@
+package tr.edu.medipol.yazgel;
+import java.math.BigDecimal;
+
+public interface Hesap {
+  
+  public String sonucuBul(String sayi1, String sayi2);
+  
+}
---
+++ HesaplamaApp/src/main/java/tr/edu/medipol/yazgel/Toplama.java
@@ -0,0 +1,15 @@
+package tr.edu.medipol.yazgel;
+import java.math.BigDecimal;
+
+public class Toplama implements Hesap {
+  
+  @Override
+  public String sonucuBul(String sayi1, String sayi2) {
+    return new BigDecimal(sayi1)
+        .add(new BigDecimal(sayi2))
+        .toString();  
+    }
+  
+  
+
+}
